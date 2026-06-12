b3256adeaccbaf6f2d20dc4fc63335e424450620
Zoya qureshi <qureshijoya1@gmail.com>
2026-06-12 08:25:07 +0530

    Add files via upload

---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore, doc, getDocFromCache, getDocFromServer } from 'firebase/firestore';
+import firebaseConfig from '../../firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const auth = getAuth(app);
+
+// Test connection strictly
+async function testConnection() {
+  try {
+    await getDocFromServer(doc(db, 'test', 'connection'));
+  } catch (error) {
+    if(error instanceof Error && error.message.includes('the client is offline')) {
+      console.error("Please check your Firebase configuration or connectivity.");
+    }
+    // Ignore other errors like permission denied for the test doc
+  }
+}
+testConnection();
